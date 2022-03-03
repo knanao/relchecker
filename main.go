775e4f4c259ce749c67be98a01d1115da0ca80b0
knanao <nao.7ken@gmail.com>
2022-03-03 20:45:59 +0900

    Add number interface

--- main.go
+++ main.go
@@ -2,25 +2,47 @@ package main
 
 import "fmt"
 
+type Number interface {
+	int | int32 | int64 | float32 | float64
+}
 
-func SumNumber[K comparable, V int64 | float64](m map[K]V) V {
-    var s V
-    for _, v := range m {
-        s += v
-    }
-    return s
+func SumNumber[K comparable, V Number](m map[K]V) V {
+	var s V
+	for _, v := range m {
+	    s += v
+	}
+	return s
 }
 
 func main() {
-	ints := map[string]int64{
+	ints := map[string]int{
 		"key1": 10,
 		"key2": 100,
 	}
-	fmt.Printf("Sum int64 value: %v\n", SumNumber(ints))
+	fmt.Printf("Sum int value: %v\n", SumNumber(ints))
 
-	floats := map[string]float64{
-		"key1": 10.0,
-		"key2": 100.0,
+	int32s := map[string]int32{
+		"key1": 20,
+		"key2": 200,
 	}
-	fmt.Printf("Sum float64 value: %v\n", SumNumber(floats))
+	fmt.Printf("Sum int32 value: %v\n", SumNumber(int32s))
+
+	int64s := map[string]int64{
+		"key1": 30,
+		"key2": 300,
+	}
+	fmt.Printf("Sum int64 value: %v\n", SumNumber(int64s))
+
+	float32s := map[string]float64{
+		"key1": 40.1,
+		"key2": 400.1,
+	}
+	fmt.Printf("Sum float32 value: %v\n", SumNumber(float32s))
+
+
+	float64s := map[string]float64{
+		"key1": 50.2,
+		"key2": 500.2,
+	}
+	fmt.Printf("Sum float64 value: %v\n", SumNumber(float64s))
 }
